f0a2e33875ed12c089fd6383dcd5874c26794b40
cqeta1564 <cqeta1564@gmail.com>
2025-03-03 14:16:31 +0100

    Added data basic help command.

--- src/command/Pomoc.java
+++ src/command/Pomoc.java
@@ -3,7 +3,18 @@ package command;
 public class Pomoc extends Command {
     @Override
     public String execute() {
-        return "";
+        return """
+                Dostupné příkazy:
+                jdi <místnost> - přesune hráče do zadané místnosti
+                konec - ukončí hru
+                koupit <množství> - nakoupí kontrabant
+                krast <předmět> - ukradne vybavení z aktuální místonosti
+                mluv <osoba> - zahájí dialog s postavou
+                nápověda - vypíše nápovědu jak dále ve hře pokračovat
+                pomoc - vypíše příkazy
+                prodat <množství/předmět> - prodá kontrabant nebo ukradené vybavení
+                progress - zobrazí postup ve hře
+                """;
     }
 
     @Override
