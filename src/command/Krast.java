0563dfbec1e2cd892174d9e949b0ff841a1e5877
cqeta1564 <cqeta1564@gmail.com>
2025-03-18 21:40:49 +0100

    Added method for stealing school stuff.

--- src/command/Krast.java
+++ src/command/Krast.java
@@ -1,11 +1,41 @@
 package command;
 
+import item.Item;
+import item.ItemFactory;
+import player.Hrac;
 import singleton.Mistnost;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Krast extends Command {
+
+    private static final Random random = new Random();
+
     @Override
-    public String execute() {
-        return "";
+    public String execute(Hrac hrac, Mistnost currentMistnost, Scanner scanner) {
+        // Možnost krádeže je dostupná jen v určitých místnostech
+        if (!isStealAllowed(currentMistnost)) {
+            return "Zde není co ukrást.\n";
+        }
+
+        System.out.println("Chceš ukrást něco? (ano/ne)");
+        String odpoved = scanner.nextLine().trim().toLowerCase();
+        if (!odpoved.equals("ano")) {
+            return "Rozmyslel sis to.\n";
+        }
+
+        if (random.nextInt(100) < 30) { // 30% šance, že hráč bude přistižen
+            return "Byl jsi přistižen při krádeži! Šatnář má podezření.\n";
+        }
+
+        Item ukradenyItem = ItemFactory.createItem("cigarety"); // Lze rozšířit o různé předměty
+        hrac.getInventar().addItem(ukradenyItem);
+        return "Ukradl jsi: " + ukradenyItem.getNazev() + "\n";
+    }
+
+    private boolean isStealAllowed(Mistnost mistnost) {
+        return mistnost.getName().equals("sklad") || mistnost.getName().equals("trida") || mistnost.getName().equals("chodba");
     }
 
     @Override
