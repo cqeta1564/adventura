d1802eb453a55d1fa3501ffe7906e310d5dbcf78
cqeta1564 <cqeta1564@gmail.com>
2025-03-18 20:54:15 +0100

    It seems, that implementation of this command will not be needed, but I do not know if I can just delete it, or I must complete it, because I already added it to game design.

--- src/command/Mluv.java
+++ src/command/Mluv.java
@@ -1,10 +1,14 @@
 package command;
 
+import player.Hrac;
 import singleton.Mistnost;
 
+import java.util.Scanner;
+
+//TODO: Nebude mozna ani za potrebi. Command tu byl z duvodu satnare, ktery si nas stejne hlida a pokud si neceho vsimne vtahne nas do nuceneho rozhovoru, kde neni potreba specifikovat prikaz mluv a v pripade questu reditele se dialog automaticky spusti pri vstupu do reditelny.
 public class Mluv extends Command {
     @Override
-    public String execute() {
+    public String execute(Hrac hrac, Mistnost currentMistnost, Scanner scanner) {
         return "";
     }
 
