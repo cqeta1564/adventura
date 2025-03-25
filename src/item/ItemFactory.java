9b3fcb13beb9894c38c5c3f18eebd0b21abe2c94
cqeta1564 <cqeta1564@gmail.com>
2025-03-25 17:30:02 +0100

    Added all items that can be in players inventory

--- src/item/ItemFactory.java
+++ src/item/ItemFactory.java
@@ -1,20 +1,17 @@
 package item;
 
-import command.Command;
-
-import java.util.HashMap;
-
 public class ItemFactory {
 
-    private HashMap<String, Command> mapa; //TODO: Vlozit vsechy kontrabandy a kradene zbozi.
     public static Item createItem(String type) {
         switch (type) {
-            case "cigarety":
-                return new Kontraband("Cigarety", 100);
-            case "alkohol":
-                return new Kontraband("Alkohol", 200);
-            case "drogy":
-                return new Kontraband("Drogy", 500);
+            case "kontraband":
+                return new Kontraband("Kontraband", 150);
+            case "promitacka":
+                return new Kontraband("Promitacka", 500);
+            case "pocitac":
+                return new Kontraband("Pocitac", 1000);
+            case "monitor":
+                return new Kontraband("Monitor", 400);
             default:
                 return null;
         }
