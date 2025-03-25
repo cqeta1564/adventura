eaf46f69194902acf51a5db8ccfddadf858b0815
cqeta1564 <cqeta1564@gmail.com>
2025-03-25 19:19:43 +0100

    Added method for removing items

--- src/player/Inventar.java
+++ src/player/Inventar.java
@@ -3,6 +3,7 @@ package player;
 import item.Item;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Inventar {
     private List<Item> items = new ArrayList<>();
@@ -12,14 +13,22 @@ public class Inventar {
         items.add(item);
     }
 
-    public void removeItem(Item item) {
-        items.remove(item);
+    public void removeItem(String item, int pocet) {
+        for (int i = 0; i < pocet; i++) {
+            for (int j = 0; j < items.size(); j++) {
+                if (Objects.equals(items.get(j).getNazev(), item)) {
+                    items.remove(j);
+                    j = items.size() - 1; //ingnorovat zbytek smycky, aby se item nevymazal vickrat, nez ma
+                }
+            }
+        }
     }
 
     public Item getItem(int index){
         return items.get(index);
     }
 
+    //TODO: Udelat smysluplnejsi vypis
     public String showItems() {
         StringBuilder sb = new StringBuilder();
         for (Item item : items) {
@@ -30,6 +39,16 @@ public class Inventar {
 
     public int getSize(){
         return items.size();
+    }
+
+    public int pocetItemu(String nazev){
+        int pocet = 0;
+        for (int i = 0; i < this.items.size(); i++) {
+            if (this.items.get(i).getNazev().equals(nazev)) {
+                pocet++;
+            }
+        }
+        return pocet;
     }
 
     //TODO: Dopsat metodu pro kontrolu kontrabandu v inventari a jeho nasledne mazani, popripade prida quest na navstevu reditelny
