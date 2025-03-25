ee862477ad16ede7a629977457cab67e9ba7f8f7
cqeta1564 <cqeta1564@gmail.com>
2025-03-25 17:29:11 +0100

    Rewrote code more nicely

--- src/command/Krast.java
+++ src/command/Krast.java
@@ -13,17 +13,31 @@ public class Krast extends Command {
 
     private static final Random random = new Random();
 
+    //TODO: Opravit chybu, kdy muze hrac ukrast veci z jine mistnosti
     @Override
     public String execute(Hrac hrac, Mistnost currentMistnost, Scanner scanner, Observable observable, String druheSlovo) {
 
-        if (random.nextInt(100) < 30) { // 30% šance, že hráč bude přistižen
+        // Kontrola mistnosti
+        if (!(currentMistnost.getName().equals("pocitacova ucebna") || currentMistnost.getName().equals("fyzikalni ucebna"))) {
+            return "Tady nemůžeš krást";
+        }
+
+        System.out.println("> Co chceš ukrást?");
+
+        String itemName = scanner.nextLine();
+
+        if (random.nextInt(100) < 50) { // 50% šance, že hráč bude přistižen
             System.out.println("> Šatnář: Co to bylo?");
             observable.notifyObservers("kradez");
         }
 
-        Item ukradenyItem = ItemFactory.createItem("cigarety"); // Lze rozšířit o různé předměty
-        hrac.getInventar().addItem(ukradenyItem);
-        return "Ukradl jsi: " + ukradenyItem.getNazev() + "\n";
+        Item ukradenyItem = ItemFactory.createItem(itemName);
+        if (!(ukradenyItem == null)) {
+            hrac.getInventar().addItem(ukradenyItem);
+            return "Ukradl jsi: " + ukradenyItem.getNazev();
+        }
+
+        return "Takový předmět neexistuje";
     }
 
     @Override
