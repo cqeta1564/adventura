3bf0a4a1f32a95ae4b72448015d2b928943c32cc
cqeta1564 <cqeta1564@gmail.com>
2025-03-25 20:26:29 +0100

    Removed TODO

--- src/command/Progress.java
+++ src/command/Progress.java
@@ -6,7 +6,6 @@ import singleton.Mistnost;
 
 import java.util.Scanner;
 
-//TODO: Nebude mozna ani za potrebi. Command tu byl z duvodu satnare, ktery si nas stejne hlida a pokud si neceho vsimne vtahne nas do nuceneho rozhovoru, kde neni potreba specifikovat prikaz mluv a v pripade questu reditele se dialog automaticky spusti pri vstupu do reditelny.
 public class Progress extends Command {
     @Override
     public String execute(Hrac hrac, Mistnost currentMistnost, Scanner scanner, Observable observable, String druheSlovo) {
