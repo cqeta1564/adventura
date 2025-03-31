5fae89da8048055c50dc3ff9a7a062013d61401a
cqeta1564 <cqeta1564@gmail.com>
2025-03-31 15:03:31 +0200

    Added basic code of Unit tests. There are not any real tests yet

--- src/UnitTests.java
+++ src/UnitTests.java
@@ -1,3 +1,35 @@
+import command.konzole.Konzole;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 public class UnitTests {
+
+    Konzole konzole;
+
+    @org.junit.jupiter.api.BeforeEach
+    void init() {
+        this.konzole = new Konzole();
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void overeniFunkcnostiTestovaniTridy() {
+        assertEquals(1, 1);
+    }
+
+    @org.junit.jupiter.api.Test
+    void overeniVytvoreniKonzole() {
+        assertNotNull(konzole);
+    }
+
+    //TODO: S timhle neco vymyslet, protoze to bude potreba, ale zatim jak presne to udelat
+    @org.junit.jupiter.api.Test
+    void overeniSpusteniKonzole() {
+        konzole.start();
+    }
+
+    @org.junit.jupiter.api.Test
+    void overeniVytvoreniHrace() {
+        assertNotNull(konzole.getHrac());
+    }
 }
