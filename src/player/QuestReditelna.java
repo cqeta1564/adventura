d84024b6babe9dba33ac0522a7949f741450691f
cqeta1564 <cqeta1564@gmail.com>
2025-04-02 11:12:01 +0200

    Moved Unit test to right place and marked res folder as resource folder

--- src/player/QuestReditelna.java
+++ src/player/QuestReditelna.java
@@ -1,6 +1,13 @@
 package player;
 
+import java.util.Scanner;
+
 //TODO: Dopsat a domyslet, jak potrestat hrace za nacapany kontraband v inventari. Videl jsem na internetu, ze se pro takovyhle samostatny questy da udelat samostatne tridy.
 public class QuestReditelna {
+
+    public QuestReditelna(Scanner sc) {
+        System.out.println("Hele koho to tu máme! (enter to continue)");
+        sc.nextLine();
+    }
 
 }
